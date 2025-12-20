ab64606508d5959bac1ec05cb741699bba4c9edd
Pawan jeswani <pwnjeswani@gmail.com>
2025-12-20 16:34:03 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.google.devtools.ksp") version "2.3.0-1.0.31"
+        id("com.google.dagger.hilt.android") version "2.57.2"
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PawanJeswaniTask"
+include(":app")
+ 
